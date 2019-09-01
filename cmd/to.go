6b6495f59ec78dd3894199a0d40de25cf755360f
Garrett Wezniak <gaarj4@gmail.com>
2019-08-31 19:15:41 -0500

    Reword user-facing text

--- cmd/to.go
+++ cmd/to.go
@@ -18,7 +18,7 @@ var toCmd = &cobra.Command{
   For example, "lend to Garrett 5 Food" would record that $5 have been lent to Garrett for food.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 3 {
-			fmt.Printf("Too few arguments. Use the form `lend to Garrett 5 Food`.\n")
+			fmt.Printf("Too few arguments. Use the form `lend to <name> <amount> <reason>`.\n")
 			os.Exit(1)
 		}
 
